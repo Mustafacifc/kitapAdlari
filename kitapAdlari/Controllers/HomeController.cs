447e81b41da065978c27489027f1d927227488cc
mustafa471 <mustafa471@BPC12>
2024-02-23 10:03:59 +0300

    Proje dosyası ekle.

---
+++ kitapAdlari/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using kitapAdlari.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace kitapAdlari.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            KitapModel kitap = new KitapModel();
+            kitap.KitapId = 1;
+            kitap.KitapAdi = "Cemil Reloaded";
+            kitap.sayfaSayisi = 1233;
+            kitap.yazari = " Reha Çamuroğlu";
+            kitap.turu = "Roman";
+
+
+            KitapModel kitap1 = new KitapModel();
+            kitap.KitapId = 2;
+            kitap.KitapAdi = "Mustafa Çiftçi Hayatı";
+            kitap.sayfaSayisi = 1233;
+            kitap.yazari = "Mustafa Çiftçi";
+            kitap.turu = "Roman";
+            ViewBag.Kitaplar = kitap;
+            ViewBag.Kitaplar1 = kitap1;
+
+            return View();
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ kitapAdlari/Models/KitapModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace kitapAdlari.Models
+{
+    public class KitapModel
+    {
+        public int KitapId { get; set; }
+        public string? KitapAdi { get; set; }
+        public int sayfaSayisi { get; set; }
+        public string? yazari { get; set; }
+        public string? turu { get; set; }
+    }
+}
